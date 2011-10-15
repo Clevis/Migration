08609286f10adc64aaa978518069ad86fc4766f9
PetrP <prochazkapp@gmail.com>
2011-10-15 19:31:20 +0200

    Libs: dump

---
+++ libs/dump.php
@@ -0,0 +1,19 @@
+<?php
+
+use \Nette\Diagnostics\Debugger;
+
+/** Dump */
+function d($var)
+{
+	foreach (func_get_args() as $var) Debugger::dump($var);
+	return func_get_arg(0);
+}
+
+/** Bar dump */
+function dd($var)
+{
+	if (func_num_args() > 1) $var = func_get_args();
+	else if (is_array($var)) $var = array(NULL => $var);
+	Debugger::barDump($var);
+	return func_get_arg(0);
+}
