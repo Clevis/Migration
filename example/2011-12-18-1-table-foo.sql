9e10e40951fcf538bff9ac5d3f97276dd6b9eafb
PetrP <prochazkapp@gmail.com>
2011-11-15 17:05:48 +0100

    Example

---
+++ example/2011-12-18-1-table-foo.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `foo` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`foo` char(3) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
---
+++ example/2011-12-18-2-table-bar.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `bar` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`bar` char(3) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
