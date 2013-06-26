669b0869f369f5b71473e59ad02336f301b24486
Vlasta Neubauer <paranoiq@centrum.cz>
2013-06-26 15:16:03 +0200

    removed examples

--- example/2011-12-18-1-table-foo.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE `foo` (
-	`id` bigint(20) NOT NULL AUTO_INCREMENT,
-	`foo` char(3) NOT NULL DEFAULT '',
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
--- example/2011-12-19-foo-to-bar-and-bar-to-foo.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE `foo`
-ADD `bar` char(3) NOT NULL DEFAULT '' AFTER `foo`;
-
-ALTER TABLE `bar`
-ADD `foo` char(3) NOT NULL DEFAULT '' AFTER `bar`;
