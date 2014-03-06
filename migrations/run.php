8b9d4da25f6ca7f08aeeccb232481d17ddb64631
Vlasta Neubauer <paranoiq@centrum.cz>
2014-03-06 17:43:33 +0100

    fix notice in Nette 2.1; add dibi.php extension by default

--- migrations/run.php
+++ migrations/run.php
@@ -1,6 +1,7 @@
 <?php
 
 use Migration\Extensions\OrmPhp;
+use Migration\Extensions\DibiPhp;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/config/Configurator.php';
@@ -19,8 +20,9 @@ if ($reset AND $context->parameters['productionMode'])
 	throw new Exception('Reset není povolen na produkčním prostředí.');
 }
 
-$runner = new Migration\Runner($context->dibiConnection);
-$runner->addExtension(new OrmPhp($configurator, $context, $context->dibiConnection));
+$runner = new Migration\Runner($dibi = $context->getService('dibiConnection'));
+$runner->addExtension(new DibiPhp($dibi));
+$runner->addExtension(new OrmPhp($configurator, $context, $dibi));
 
 $finder = new Migration\Finders\MultipleDirectories;
 $finder->addDirectory(__DIR__ . '/struct');
